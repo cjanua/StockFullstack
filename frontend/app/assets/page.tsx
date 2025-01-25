460c7d283c10b22d9437a03c1e0c1a2f4ea443e7
Chris Jan <me@cjanua.xyz>
2025-01-24 20:14:25 -0500

    Chart Styling

--- frontend/app/assets/page.tsx
+++ frontend/app/assets/page.tsx
@@ -1,16 +1,26 @@
 "use client"
 import { SymbolOverviewNoSSR } from "@/components/plotting/SymbolOverview";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const viewHeight = document.body.clientHeight * 2 / 3;
-  const viewWidth = document.body.clientWidth * 0.75;
+  const [ view, setView ] = useState({ height: document.body.clientHeight, width: document.body.clientWidth * 0.75 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setView({ height: document.body.clientHeight, width: document.body.clientWidth * 0.75 });
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="grid place-items-center pt-8">
       <SymbolOverviewNoSSR
         dateFormat="MM/dd/yy"
         colorTheme="dark"
-        height={viewHeight}
-        width={viewWidth}
+        height={view.height}
+        width={view.width}
         chartType="candlesticks"
         downColor="#800080"
         borderDownColor="#800080"
